0854b8c1f899cd959db971f3ac32516e0871568c
Octavio Lugo Camacho <octaviolugocamacho@gmail.com>
2024-12-19 14:49:31 -0600

    [ADD] Add validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,10 @@
 class Car < ApplicationRecord
+  validates :plate_number, uniqueness: {
+    message: "La placa %{value} ya existe en la Base de Datos."
+  }
+  validates :year, numericality: {
+    greater_than_or_equal_to: 1900,
+    less_than_or_equal_to: ->(_brewery) { Date.current.year },
+    message: "Solo se admiten autos entre el año 1900 y el año actual."
+  }
 end
