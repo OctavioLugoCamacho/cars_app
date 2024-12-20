ff86293f46e2833809c58ca3fa323623ef99f6b7
Octavio Lugo Camacho <octaviolugocamacho@gmail.com>
2024-12-20 12:57:30 -0600

    [ADD] Add model with validations for services

---
+++ app/models/maintenance_service.rb
@@ -0,0 +1,7 @@
+class MaintenanceService < ApplicationRecord
+  belongs_to :car
+
+  validates_presence_of :description, message: "- Favor de introducir la descripción."
+
+  validates_comparison_of :date,  less_than_or_equal_to: -> { Date.today }, message: "- La fecha y hora deben ser menor o igual a la fecha y hora actual."
+end
