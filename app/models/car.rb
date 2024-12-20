d16cc8c2c538dd8ddff99f932ea411931554bce4
Octavio Lugo Camacho <octaviolugocamacho@gmail.com>
2024-12-20 12:52:44 -0600

    [ADD] Add relation has_many in cars

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,12 +1,16 @@
 class Car < ApplicationRecord
+  has_many :maintenance_services, dependent: :destroy
+
   validates :plate_number, uniqueness: {
     message: "- La placa %{value} ya existe en la Base de Datos."
   }
+
   validates :year, numericality: {
     only_integer: true,
     greater_than_or_equal_to: 1900,
     less_than_or_equal_to: ->(_brewery) { Date.current.year },
     message: "- Solo se admiten autos entre el año 1900 y el año actual."
   }
+
   validates_presence_of :model, message: "- Favor de introducir el modelo."
 end
