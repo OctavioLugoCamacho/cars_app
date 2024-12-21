93c63559a214930a52cb7efaaadc061117c931e3
Octavio Lugo Camacho <octaviolugocamacho@gmail.com>
2024-12-21 15:11:35 -0600

    [ADD] Add delete function in maintenance service

--- app/controllers/maintenance_services_controller.rb
+++ app/controllers/maintenance_services_controller.rb
@@ -1,15 +1,15 @@
 class MaintenanceServicesController < ApplicationController
+  before_action :find_car, only: [ :new, :create ]
+
   def index
     @maintenance_services = MaintenanceService.all
   end
 
   def new
-    @car = Car.find(params[:car_id])
     @maintenance_service = @car.maintenance_services.new
   end
 
   def create
-    @car = Car.find(params[:car_id])
     @maintenance_service = @car.maintenance_services.new(maintenance_service_params)
     if @maintenance_service.save
       redirect_to @car, notice: "Se ha registrado un nuevo servicio"
@@ -18,9 +18,20 @@ class MaintenanceServicesController < ApplicationController
     end
   end
 
+  def destroy
+    @car = Car.find(params[:id])
+    @maintenance_service = MaintenanceService.find(params[:car_id])
+    @maintenance_service.destroy
+    redirect_to @car, alert: "Servicio eliminado satisfactoriamente!"
+  end
+
 private
 
   def maintenance_service_params
     params.require(:maintenance_service).permit(:description, :status, :date)
+  end
+
+  def find_car
+    @car = Car.find(params[:car_id])
   end
 end
