6c16aab2535321f78b37b9290d89e4bb6a67f852
Octavio Lugo Camacho <octaviolugocamacho@gmail.com>
2024-12-23 09:34:00 -0600

    [ADD] Add Enum in status

--- app/models/maintenance_service.rb
+++ app/models/maintenance_service.rb
@@ -4,4 +4,6 @@ class MaintenanceService < ApplicationRecord
   validates_presence_of :description, message: "- Favor de introducir la descripción."
 
   validates_comparison_of :date,  less_than_or_equal_to: -> { Date.today }, message: "- La fecha y hora deben ser menor o igual a la fecha y hora actual."
+
+  enum :status, { pending: 0, in_progress: 1, completed: 2 }
 end
